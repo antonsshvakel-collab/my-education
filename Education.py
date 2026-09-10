b2edab8c4d7915da0188c3bf2c1de8d42aef54be
Slardar 30 tier <antonsshvakel-collab@users.noreply.github.com>
2026-09-10 21:45:02 +0300

    feat: add print hello amd print a

---
+++ Education.py
@@ -0,0 +1,22 @@
+import math
+import random
+import os
+import shutil
+import builtins
+import cmath
+import re
+import keyword
+import asyncio
+from datetime import datetime, date
+import pytz
+import time
+import threading
+
+
+
+print("Hello")
+a=10
+print(a)
+
+
+
